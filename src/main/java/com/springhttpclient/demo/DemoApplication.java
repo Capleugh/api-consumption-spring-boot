85602490a74b7ad5a02c0729649a85aa3dae334b
Carleigh Crockett <capleugh@gmail.com>
2020-11-03 02:45:29 -0700

    add GET request and parse JSON

--- src/main/java/com/springhttpclient/demo/DemoApplication.java
+++ src/main/java/com/springhttpclient/demo/DemoApplication.java
@@ -1,13 +1,36 @@
 package com.springhttpclient.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+	private static final String POSTS_API_URL = "http://jsonplaceholder.typicode.com/posts";
 
+	public static void main(String[] args) throws IOException, InterruptedException {
+		SpringApplication.run(DemoApplication.class, args);
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.GET()
+				.header("accept", "application/json")
+				.uri(URI.create(POSTS_API_URL))
+				.build();
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+//	parse JSON into objects
+		ObjectMapper mapper = new ObjectMapper();
+		List<Post> posts = mapper.readValue(response.body(), new TypeReference<List<Post>>() {});
+
+		posts.forEach(System.out::println);
+	}
 }
